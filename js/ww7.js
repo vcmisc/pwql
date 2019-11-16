c01adcf88fbb369e7230dcc2f40af74aba3dafd3
sandpipes <17598972+sandpipes@users.noreply.github.com>
2019-11-15 20:49:35 -0500

    .

--- js/ww7.js
+++ js/ww7.js
@@ -124,7 +124,7 @@ function question15(){
     document.getElementById("q15.d").innerHTML = "F4 = 0i " + clen*magfield*7*turns + "j + 0k N";
 
 
-    document.getElementById("q15.d").innerHTML = "M = " + clen*alen*7*turns*Math.sin(angle* Math.PI / 180) + "i + " + clen*alen*7*turns*Math.sin((90-angle)* Math.PI / 180) + "j + 0k A * m^2";
-    document.getElementById("q15.d").innerHTML = "M = 0i + 0j + " + magfield*alen*clen*7*turns*Math.sin((90-angle)* Math.PI / 180) + "k Nm";
+    document.getElementById("q15.e").innerHTML = "M = " + clen*alen*7*turns*Math.sin(angle* Math.PI / 180) + "i + " + clen*alen*7*turns*Math.sin((90-angle)* Math.PI / 180) + "j + 0k A * m^2";
+    document.getElementById("q15.f").innerHTML = "M = 0i + 0j + " + magfield*alen*clen*7*turns*Math.sin((90-angle)* Math.PI / 180) + "k Nm";
 
 }
